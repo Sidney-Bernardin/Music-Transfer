c0e6b42f37b3298495a09b899a1eb52ed5271f36
sidney <sidney.bernardin@gmail.com>
2021-07-30 13:30:46 -0400

    Now listing all playlist items.

--- server/handlers.go
+++ server/handlers.go
@@ -4,34 +4,64 @@ import (
 	"net/http"
 
 	"github.com/sirupsen/logrus"
+	"google.golang.org/api/googleapi"
 )
 
-func respond(w http.ResponseWriter, r *http.Request, err error, code int) {
+func erro(w http.ResponseWriter, r *http.Request, err error, code int) {
 	if err != nil {
 		logrus.Warnf("%s %s\n\t╚ %v) %v", r.Method, r.URL.String(), code, err)
 		http.Error(w, err.Error(), code)
 		return
 	}
-
-	logrus.Infof("%s %s\n\t %v", r.Method, r.URL.String(), code)
 }
 
 func (s *server) Home() http.HandlerFunc {
+
+	var response struct {
+		PlaylistID string
+		Playlist   struct {
+			ThumbnailURL string
+			Items        []struct {
+				Title        string
+				ThumbnailURL string
+			}
+		}
+		Error string
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		res, err := s.ytSrv.Playlists.List([]string{"status"}).
-			ChannelId("GoogleDevelopers").
-			//Mine(true).
-			Do()
-		if err != nil {
-			respond(w, r, err, http.StatusInternalServerError)
-			return
+		if r.Method == "POST" {
+
+			// Get the playlists of the channel.
+			res, err := s.ytSrv.PlaylistItems.List([]string{"snippet", "contentDetails"}).
+				PlaylistId(r.FormValue("PlaylistID")).
+				MaxResults(200).
+				Do()
+
+			if err != nil {
+
+				// Check for a channel not found error.
+				if err.(*googleapi.Error).Errors[0].Reason == "channelNotFound" {
+					response.Error = "channel not found"
+				} else {
+					erro(w, r, err, http.StatusInternalServerError)
+					return
+				}
+			} else {
+				for _, v := range res.Items {
+					response.Playlist.Items = append(response.Playlist.Items, struct {
+						Title        string
+						ThumbnailURL string
+					}{v.Snippet.Title, v.Snippet.Thumbnails.Medium.Url})
+				}
+			}
 		}
 
 		// Execute the home template.
-		respond(w, r, nil, http.StatusOK)
-		if err := s.tpl.ExecuteTemplate(w, "home.html", res.Items[0]); err != nil {
-			respond(w, r, err, http.StatusInternalServerError)
+		logrus.Infof("%s %s\n\t╚ %v", r.Method, r.URL.String(), http.StatusOK)
+		if err := s.tpl.ExecuteTemplate(w, "home.html", response); err != nil {
+			erro(w, r, err, http.StatusInternalServerError)
 			return
 		}
 	}
